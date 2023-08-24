789e47d311d3cadbbae1ddd2691717ae40d4ed13
Max <maxdahlgren94@gmail.com>
2023-08-24 10:59:43 +0200

    Fixed swagger in Program.cs

--- Program.cs
+++ Program.cs
@@ -9,15 +9,12 @@ builder.Services.AddSwaggerGen();
 
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
+app.UseSwagger();
+app.UseSwaggerUI(c =>
 {
-    app.UseSwagger();
-    app.UseSwaggerUI(c =>
-    {
-        c.SwaggerEndpoint("/swagger/v1/swagger.json", "WestCoast Education");
-    });
-}
+    c.SwaggerEndpoint("/swagger/v1/swagger.json", "WestCoast Education");
+    c.RoutePrefix = string.Empty;
+});
 
 app.UseHttpsRedirection();
 
