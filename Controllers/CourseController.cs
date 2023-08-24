6f748e01bd8b0f19f81254da9247b8dc7d1e91b5
Max <maxdahlgren94@gmail.com>
2023-08-24 10:43:18 +0200

    Finished web api

---
+++ Controllers/CourseController.cs
@@ -0,0 +1,29 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Westcoast_education_cicd.Entities;
+
+namespace Westcoast_education_cicd.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class CourseController : ControllerBase
+    {
+        private readonly List<Course> courses = new List<Course>
+        {
+            new Course {Id = 1, Name = "Math" },
+            new Course {Id = 2, Name = "English" },
+            new Course {Id = 2, Name = "Science" },
+            new Course {Id = 2, Name = "Biology" },
+            new Course {Id = 2, Name = "Computer Science" },
+            new Course {Id = 2, Name = "History" }
+        };
+        [HttpGet]
+        public ActionResult<IEnumerable<Course>> Get()
+        {
+            return courses;
+        }
+    }
+}
